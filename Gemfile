8bce1ce30cc26f9db862855766bea88a9bd98396
EC2 Default User <ec2-user@ip-172-31-2-112.ap-northeast-1.compute.internal>
2023-08-03 09:31:47 +0000

    [update]8/3

--- Gemfile
+++ Gemfile
@@ -56,4 +56,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
 
+gem 'jquery-rails'
+
 gem 'net-smtp'
--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,18 +1,18 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
   end
 
   def destroy
-    BookComment.find(params[:book_id]).destroy
-    redirect_to book_path(params[:id])
+    @book = Book.find(params[:book_id])
+    BookComment.find(params[:id]).destroy
   end
 
-   private
+  private
 
   def book_comment_params
     params.require(:book_comment).permit(:comment)
